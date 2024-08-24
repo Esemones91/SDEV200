cb2d32d84f37ec15ef45f025fc858a920418a4ee
Esemones91 <eeesemones@gmail.com>
2024-08-23 21:36:42 -0400

    Add files via upload

---
+++ M01/MovieQuoteInfo.java
@@ -0,0 +1,17 @@
+// Filename: MovieQuoteInfo
+// Written by Erick Semones
+// WRitten on 8-23-2024
+/*This class uses the println() Method to
+ * dispaly my favorite movie quote, the
+ * movie it is from, the character who says
+ * the quote, and the year the movie was 
+ * made.*/
+public class MovieQuoteInfo {
+    public static void main(String[] args) 
+    {
+        System.out.println("Movie Quote: Multipass.");
+        System.out.println("Movie: The Fifth Element");
+        System.out.println("Character: Leelu");
+        System.out.println("Year: 1997");
+    }
+}
