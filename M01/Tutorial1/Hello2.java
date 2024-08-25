8d6f22d2d24c6d92b19706ce5dfefaac8810d56d
Esemones91 <eeesemones@gmail.com>
2024-08-25 17:29:12 -0400

    Add files via upload

---
+++ M01/Tutorial1/Hello2.java
@@ -0,0 +1,13 @@
+package Tutorial1;
+//filename Hello2.java
+//written by Erick Semones
+//written on 8-22-2024
+public class Hello2 
+/*This class demonstrates the use of the println() 
+ * Method to print the message hello, world!*/
+{
+    public static void main(String[] args) 
+    {
+        System.out.println("Hellow, world!");
+    }
+}
---
+++ M01/Tutorial1/HelloDialogue.java
@@ -0,0 +1,14 @@
+package Tutorial1;
+
+import javax.swing.JOptionPane;
+
+//file name HelloDialogue.java
+//written by Erick Semones
+//written on 8-22-2024
+public class HelloDialogue 
+{
+    public static void main(String[] args)
+    {
+        JOptionPane.showMessageDialog(null, "Hellow, world!");
+    }    
+}
